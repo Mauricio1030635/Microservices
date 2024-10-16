a3ffc66833618df82a71f76ebc266e96f8a3a2b1
MMS <mauricio@hotmail.com>
2024-10-15 22:58:53 -0500

    Estrutura basica

---
+++ src/MMS_Services/Services/Catalog/Catalog.API/Model/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.API.Model
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        //(Asignación de valor por defecto con operador de supresión de advertencias)
+        //!: Este símbolo es el operador de supresión de advertencias de nulabilidad.
+        public string Name { get; set; } = default!;
+        //siempre estará inicializada con una nueva lista vacía
+        public List<string> Category { get; set; } = new();
+        public string Description { get; set; } = default!;
+        public string ImageFile{ get; set; } = default!;
+        public decimal Price { get; set; } = default!;
+
+
+
+    }
+}
